0c3c0f0582bf11ba7ebb62622386220ac21c4388
Sinju Zacharias <sinjuzacharias1992@gmail.com>
2023-10-03 17:55:51 +0530

    User1 first commit into master branch

---
+++ src/n.java
@@ -0,0 +1,26 @@
+
+public class n {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		{
+			   int marks=55;
+			    if(marks >= 80)
+			    {
+			       System.out.println("DISTINCTION");
+			    }
+
+			     else if(marks >=35)
+			        System.out.println("PASS");
+			  else
+			   System.out.println("FAIL");
+
+			}
+
+		
+		
+	}
+
+}
